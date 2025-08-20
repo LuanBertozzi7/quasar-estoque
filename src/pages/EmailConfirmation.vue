ea1d3cfdd0ba75d02b1fc3ca8baacf7cce6cd216
= <LuanBertozziS2@hotmail.com>
2025-08-19 22:29:08 -0400

    feat: email-confirmation ready

---
+++ src/pages/EmailConfirmation.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <p class="text-body1">
+      thanks for register !
+    </p>
+    <p class="text-body1">
+      please confirm your email to finishing register at <strong>{{ $route.query.email }}</strong>
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation',
+})
+</script>
